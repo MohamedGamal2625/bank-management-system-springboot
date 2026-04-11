e05fada4cbb7a90600c3eec8a486b37d34e2de04
Mohamed Gamal <mohamedgamaleng74@gmail.com>
2026-04-11 06:18:26 +0200

    commit

--- src/main/java/com/global/bankingsystemapi/entity/Transaction.java
+++ src/main/java/com/global/bankingsystemapi/entity/Transaction.java
@@ -1,6 +1,5 @@
 package com.global.bankingsystemapi.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.global.bankingsystemapi.entity.enums.TransactionType;
 import jakarta.persistence.*;
